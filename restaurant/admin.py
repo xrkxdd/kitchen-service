580e951cc015f9fa8bae369c52a7d38ebb19522f
xrkxdd <nazar.mc28@gmail.com>
2024-06-24 15:07:02 +0200

    edit admin.py

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -3,6 +3,32 @@ from django.contrib.auth.admin import UserAdmin
 from .models import DishType, Cook, Dish
 
 
+@admin.register(Cook)
+class CookAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("years_of_experience",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "years_of_experience",
+                    )
+                },
+            ),
+        )
+    )
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_filter = ("cooks",)
+
+
 admin.site.register(DishType)
-admin.site.register(Dish)
-admin.site.register(Cook, UserAdmin)

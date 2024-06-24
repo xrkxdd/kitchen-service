6139629db7229d1528b6bcf1f461afa88054069c
xrkxdd <nazar.mc28@gmail.com>
2024-06-24 15:05:00 +0200

    create superuser

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import DishType, Cook, Dish
 
-# Register your models here.
+
+admin.site.register(DishType)
+admin.site.register(Dish)
+admin.site.register(Cook, UserAdmin)
